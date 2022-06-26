7f3492092e66d02f68e729409ea5fac93aa7cdd7
frznheart <51480126+frznheart@users.noreply.github.com>
2022-06-26 14:00:53 +0545

    06/25/2022

--- server.js
+++ server.js
@@ -1,33 +1,62 @@
 const express = require("express");
+
 const app = express();
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 //MIDDLE WARE
 //global middelware
+// app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log("middleware");
+  req.body = {
+    id: 1,
+    name: "John",
+  };
+  console.log("before middleware");
   next(); //calls the next valid middleware
+  // console.log("after middelware");
 });
 
-// // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }));
-
-// // parse application/json
 // app.use(bodyParser.json());
 
+// const auth_middleware = (req, res, next) => {
+//   let auth_status = false;
+//   if (auth_status) {
+//     console.log("auth_middleware");
+//     next();
+//   } else {
+//     res.status(401);
+
+//     next({ err: "not authenticated" });
+//   }
+// };
+
+// app.use(auth_middleware);
+app.use(express.static("public"));
 app.use(express.json());
-
-//local middleware
-//third party middlware
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
 
 app.post("/", (req, res) => {
   console.log(req.body);
   res.send("POST");
 });
 
-app.listen(8000);
+app.get("/", function (req, res) {
+  console.log("hello World");
+  res.send("Hello World"); //cannot send response after sending 1 response
+  console.log(req.query);
+});
+
+app.use("", function (req, res) {
+  console.log("404");
+  res.status(404).send({ msg: "404 ERROR!! page not found" });
+});
+
+// app.use("", function (err, req, res, next) {
+//   console.log(req.body);
+//   res.send(err);
+// });
+
+app.listen(8000, (err, data) => {
+  console.log("listening");
+});
